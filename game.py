5a29de94fa371427d07ed542c5625730e159de85
Dinara <dinaragig03@gmail.com>
2020-04-25 21:08:53 +0300

    Added rating

--- game.py
+++ game.py
@@ -8,22 +8,26 @@ class Game:
         self._master_user_id = 0
         self._word_list = []
         self._current_word = ''
+        self._game_started = False
 
     def start(self):
         self._word_list = settings.word_list.copy()
-        self._current_word = self._create_word()
         self._master_user_id = 0
+        self._game_started = True
+
+    def is_game_started(self):
+        return self._game_started
 
     def set_master(self, user_id):
+        self._create_word()
         self._master_user_id = user_id
 
     def is_master(self, user_id: int):
         return user_id == self._master_user_id
 
     def _create_word(self):
-        word = random.choice(self._word_list)
-        del self._word_list[self._word_list.index(word)]
-        return word
+        self._current_word = random.choice(self._word_list)
+        del self._word_list[self._word_list.index(self._current_word)]
 
     def get_word(self, user_id: int):
         if self.is_master(user_id):
@@ -33,7 +37,7 @@ class Game:
 
     def change_word(self, user_id: int):
         if self.is_master(user_id):
-            self._current_word = self._create_word()
+            self._create_word()
             return self._current_word
         else:
             return ''
@@ -46,5 +50,3 @@ class Game:
 
     def get_current_word(self):
         return self._current_word
-
-
--- main.py
+++ main.py
@@ -7,39 +7,61 @@ from game import Game
 import settings
 
 game = Game()
+rating_dict = {}
 
 
-def start(update, context: CallbackContext):
+def command_start(update, context: CallbackContext):
     game.start()
     update.message.reply_text('Игра Крокодил началась', reply_to_message_id=True)
 
 
-def master(update: Update, context):
+def set_master(update, context):
     username = update.message.from_user.full_name
-    print('master =' + str(update))
     game.set_master(update.message.from_user.id)
     update.message.reply_text('Ведущий {}'.format(username), reply_to_message_id=True)
 
 
-def show_word(update, context):
+def command_master(update: Update, context):
+    if game.is_game_started():
+        set_master(update, context)
+
+
+def command_show_word(update, context):
     user_id = update.message.from_user.id
     word = game.get_word(user_id)
     update.message.reply_text(word, reply_to_message_id=True)
 
 
-def change_word(update, context):
+def command_change_word(update, context):
     user_id = update.message.from_user.id
     word = game.change_word(user_id)
     update.message.reply_text(word, reply_to_message_id=True)
+
+
+def command_rating(update, context):
+    rating_str = ''
+    for elem in rating_dict:
+        rating_str += elem.split()[1] + ": " + str(rating_dict[elem]) + " "
+
+    update.message.reply_text(rating_str, reply_to_message_id=True)
 
 
 def is_word_answered(update, context):
     user_id = update.message.from_user.id
     text = update.message.text
     if game.is_word_answered(user_id, text):
+        user_id = update.message.from_user.id
         username = update.message.from_user.full_name
+
+        if str(user_id) + ' ' + username in rating_dict:
+            rating_dict[str(user_id) + ' ' + username] += 1
+        else:
+            rating_dict[str(user_id) + ' ' + username] = 1
+
         word = game.get_current_word()
         update.message.reply_text('Слово {} отгадал игрок {}'.format(word, username), reply_to_message_id=True)
+
+        set_master(update, context)
 
 
 def main():
@@ -47,10 +69,11 @@ def main():
 
     dp = updater.dispatcher
 
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("master", master))
-    dp.add_handler(CommandHandler("show_word", show_word))
-    dp.add_handler(CommandHandler("change_word", change_word))
+    dp.add_handler(CommandHandler("start", command_start))
+    dp.add_handler(CommandHandler("master", command_master))
+    dp.add_handler(CommandHandler("show_word", command_show_word))
+    dp.add_handler(CommandHandler("change_word", command_change_word))
+    dp.add_handler(CommandHandler("rating", command_rating))
 
     dp.add_handler(MessageHandler(Filters.text, is_word_answered))
 
