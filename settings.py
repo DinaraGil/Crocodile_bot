dab40cfd4fe6d33b49f1d547b79b218ce118c438
Dinara <dinaragig03@gmail.com>
2020-04-28 15:25:07 +0300

    Different proxy

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-PROXY_URL = 'http://82.200.233.4:3128'
-REQUEST_KWARGS = {'proxy_url': 'http://82.200.233.4:3128'}
+PROXY_URL = 'https://54.37.131.45:3128'
+REQUEST_KWARGS = {'proxy_url': 'https://54.37.131.45:3128'}
 TOKEN = "1221980714:AAHuDcBOFq5ITPnG_kPQnQ76RxUfZStkJB4"
 with open('words.txt', 'r', encoding='utf-8') as file:
     word_list = file.read().splitlines()
